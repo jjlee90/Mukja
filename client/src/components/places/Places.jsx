8806fb8a7b8d15d2eaa0a9f60d9622509d6ba335
Joseph Lee <jlee190@hotmail.com>
2022-10-18 13:45:32 -0400

    refactored client/places/places, mapped <PlaceCard/>

--- client/src/components/places/Places.jsx
+++ client/src/components/places/Places.jsx
@@ -4,11 +4,11 @@ import Card from "./PlacesCard"
 import Loader from "../loader/Loader"
 import PlaceCard from "./PlacesCard"
 
-export default function Places() {
+export default function Places(props) {
   // usestate to set fetched food data
-  const [foodData, setFoodData] = useState({})
+  const [foodData, setFoodData] = useState([])
   const [loading, setLoading] = useState(false)
-  let data = undefined
+
   useEffect(() => {
     const fetchFoods = async () => {
       setLoading(true) //
@@ -19,38 +19,34 @@ export default function Places() {
         window.alert(message)
         return
       }
-      data = await res.json()
-      //console.log({ data })
-
+      let data = await res.json()
+      console.log(data.businesses)
       setLoading(false)
-      return data
-      //console.log(data.businesses[0])
+      setFoodData(data.businesses)
     }
-    fetchFoods().then((data) => {
-      setFoodData(data)
-      console.log(foodData)
-    })
+
+    fetchFoods()
+    return
   }, [])
+
+  let mapData = foodData.map((place) => {
+    return (
+      <PlaceCard
+        key={place.id}
+        name={place.name}
+        image={place.image_url}
+        address={place.location.address1}
+      />
+    )
+  })
+
+  console.log(foodData)
   return (
     <div className="container">
       <h3>Places</h3>
       <div className="places-container">
         <div className="card-container">
-          <PlaceCard
-            name={foodData.businesses[0].name}
-            image={foodData.businesses[0].image_url}
-            address={foodData.businesses[0].location.address1}
-          />
-          <PlaceCard
-            name={foodData.businesses[1].name}
-            image={foodData.businesses[1].image_url}
-            address={foodData.businesses[1].location.address1}
-          />
-          <PlaceCard
-            name={foodData.businesses[2].name}
-            image={foodData.businesses[2].image_url}
-            address={foodData.businesses[2].location.address1}
-          />
+          {foodData.length != 0 ? mapData : ""}
         </div>
         <div className="map-container">
           {!loading ? <Map foodData={foodData} /> : <Loader />}
--- client/src/components/places/PlacesCard.jsx
+++ client/src/components/places/PlacesCard.jsx
@@ -1,16 +1,15 @@
 import React from "react"
 
-export default function PlaceCard({image, name, address, phone}) {
-    //console.log(name)
-    return (
-        <div className="card">
-        <img src={image} alt="place image" className="holder" />
-        <div>
-          <p>{name}</p>
-          <p>{address}</p>
-          <p>{phone}</p>
-        </div>
+export default function PlaceCard({ image, name, address, phone }) {
+  // console.log(name)
+  return (
+    <div className="card">
+      <img src={image} alt="place image" className="holder" />
+      <div>
+        <p>{name}</p>
+        <p>{address}</p>
+        <p>{phone}</p>
       </div>
-    )
-
+    </div>
+  )
 }
