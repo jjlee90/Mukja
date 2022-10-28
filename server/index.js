e32d08629f71df09728a45ffcfaa941169c92a19
Joseph Lee <jlee190@hotmail.com>
2022-10-28 16:39:10 -0400

    test

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require("path")
 const bodyParser = require("body-parser")
 const methodOverride = require("method-override")
 require("dotenv").config()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 app.use(methodOverride("_method"))
 
@@ -20,9 +20,9 @@ const db = require("./models/index.db")
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"))
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  )
+  // app.get("*", (req, res) =>
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  // )
   // app.use(express.static(path.join(__dirname, "client", "build")))
 }
 
