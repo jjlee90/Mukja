c7b2ebc6662312b209fcefed431dc62dd7a61e67
Joseph Lee <jlee190@hotmail.com>
2022-10-30 03:22:18 -0400

    test

--- server/index.js
+++ server/index.js
@@ -16,15 +16,15 @@ app.use(cors())
 // console.log(path.join(__dirname, "client", "build"))
 const db = require("./models/index.db")
 
-// serve static front end in production mode
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"))
+// // serve static front end in production mode
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"))
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  )
-  // app.use(express.static(path.join(__dirname, "client", "build")))
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+//   )
+//   // app.use(express.static(path.join(__dirname, "client", "build")))
+// }
 
 db.sequelize
   .sync() // {force: false} delete force obj after dev. it drops all tables then recreates them.
