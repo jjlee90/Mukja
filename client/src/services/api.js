c80a71f58046aa96019552bcaa554e395d0d8cee
Joseph Lee <jlee190@hotmail.com>
2024-03-01 13:46:05 -0500

    up

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 let baseURL;
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
   // Running locally
   baseURL = "http://localhost:3001/api";
   // baseURL = "/.client/functions";
 } else {
   // Running on Netlify
-  baseURL = "http://localhost:3001/api"; //"http://ec2-18-209-7-174.compute-1.amazonaws.com:8080/api"; //"https://mukja.netlify.app/.netlify/functions";
+  baseURL = "https://mukja.netlify.app/api"; //"https://mukja.netlify.app/.netlify/functions";
 }
 
 const api = axios.create({
